12ef8ff3fbe9512db10c41ae4b4631bb667fc86f
MariaPaola Jimenez <pocayoki@hotmail.com>
2017-11-21 20:55:33 -0500

    My working repo

--- js/index.js
+++ js/index.js
@@ -12,11 +12,35 @@ $.ajax({
     url: 'data/shoes.json',
     type: 'GET',
   })
-  .done(function(response) {
-    console.log(response);
-  })
-  .fail(function() {
+  .done(displayShoes) 
+  .fail(function(error) {
     console.log("error");
   });
+
+  function displayShoes(response){
+    var releases = response.releases;
+    var html = '';
+    
+    $(releases).each(function(i, shoe){
+    var name = shoe.name;
+    var date = shoe.date;
+    var image = shoe.image;
+    var price = shoe.price;
+
+    html += `<div class="tile">
+        <img src="`+ image +`" alt="">
+        <div class="overlay">
+          <div class="details">
+            <p class="name">`+ name +`</p>
+            <p class="date">`+ date +`</p>
+            <p class="price">`+ price +`</p>
+          </div>
+        </div>
+      </div>`;
+    });
+
+    $('.tiles').html(html);
+
+  }
 
 });
